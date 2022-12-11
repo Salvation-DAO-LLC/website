1f9d1551f3a2c831b97187677130db6bdd676d5a
John Nguyen <jtnguyen236@gmail.com>
2022-12-11 14:09:00 +0800

    NEXT_PUBLIC

--- state/consts.ts
+++ state/consts.ts
@@ -1,5 +1,5 @@
-export const CHAIN_ID = parseInt(process.env.CHAIN_ID || "") || 5
-export const SDG_TOKEN = process.env.SDG_TOKEN || "0x44c9Ff477400ECA0Dd65Fcb880163039Bb8F2E1f"
-export const USDT = process.env.USDT || "0xFd20C7a96B90203728AdD9A0D46790C75f9DE463"
-export const BLOCK_EXPLORER_URL = process.env.BLOCK_EXPLORER_URL || "https://goerli.etherscan.io/"
-export const PROPERTY_SERVER = process.env.PROPERTY_SERVER || "https://propertyserver.supremacygame.io/api"
+export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "") || 5
+export const SDG_TOKEN = process.env.NEXT_PUBLIC_SDG_TOKEN || "0x44c9Ff477400ECA0Dd65Fcb880163039Bb8F2E1f"
+export const USDT = process.env.NEXT_PUBLIC_USDT || "0xFd20C7a96B90203728AdD9A0D46790C75f9DE463"
+export const BLOCK_EXPLORER_URL = process.env.NEXT_PUBLIC_BLOCK_EXPLORER_URL || "https://goerli.etherscan.io/"
+export const PROPERTY_SERVER = process.env.NEXT_PUBLIC_PROPERTY_SERVER || "https://propertyserver.supremacygame.io/api"
